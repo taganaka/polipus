7009b855020d9b5a42639b91e83d15ea5354b3b3
Francesco Laurita <taganaka@gmail.com>
2013-06-09 02:38:55 +0200

    A bloom filter based url tracker

---
+++ lib/polipus/url_tracker/bloomfilter.rb
@@ -0,0 +1,23 @@
+require "redis-bloomfilter"
+module Polipus
+  module UrlTracker
+    class Bloomfilter
+      def initialize(options = {})
+        @bf = Redis::Bloomfilter.new options
+      end
+
+      def visited?(url)
+        @bf.include?(url)
+      end
+
+      def visit url
+        @bf.insert url
+      end
+
+      def clear
+        @bf.clear
+      end
+
+    end
+  end
+end
