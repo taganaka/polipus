d8a0d832992d7eea94701de39dc123e31c888bdb
Francesco Laurita <taganaka@gmail.com>
2013-06-29 16:19:57 +0200

    Adding options. Might be useful in the future

--- lib/polipus/queue_overflow.rb
+++ lib/polipus/queue_overflow.rb
@@ -1,10 +1,11 @@
+require "polipus/queue_overflow/manager"
 module Polipus
   module QueueOverflow
-    def self.mongo_queue(mongo_db, queue_name)
+    def self.mongo_queue(mongo_db, queue_name, options = {})
       require "polipus/queue_overflow/mongo_queue"
       mongo_db ||= Mongo::Connection.new("localhost", 27017, :pool_size => 15, :pool_timeout => 5).db('polipus')
       raise "First argument must be an instance of Mongo::DB" unless mongo_db.is_a?(Mongo::DB)
-      self::MongoQueue.new mongo_db, queue_name
+      self::MongoQueue.new mongo_db, queue_name, options
     end
   end
 end
