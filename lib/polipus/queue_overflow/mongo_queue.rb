bca56f69ea9c2d36dffff4ddc7f1f6a70875ddc1
Francesco Laurita <taganaka@gmail.com>
2013-06-29 16:20:26 +0200

    Making the interface compatible qith redis queue

--- lib/polipus/queue_overflow/mongo_queue.rb
+++ lib/polipus/queue_overflow/mongo_queue.rb
@@ -2,10 +2,11 @@ require "thread"
 module Polipus
   module QueueOverflow
     class MongoQueue
-      def initialize(mongo_db, queue_name)
+      def initialize(mongo_db, queue_name, options = {})
         @mongo_db = mongo_db
         @collection_name = "polipus_q_overflow_#{queue_name}"
         @semaphore = Mutex.new
+        @options = options
       end
 
       def length
@@ -24,7 +25,7 @@ module Polipus
         @mongo_db[@collection_name].insert({:payload => data})
       end
 
-      def pop
+      def pop(_ = false)
         @semaphore.synchronize {
           doc = @mongo_db[@collection_name].find({},:sort => {:_id => 1}).limit(1).first
           return nil if doc.nil?
