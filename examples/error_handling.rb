fdef32000b4913a315b495ddfb52a92f1c6bd687
Tobias L. Maier <tobias.maier@baucloud.com>
2014-06-02 11:22:02 +0200

    Add example for error handling. #15

---
+++ examples/error_handling.rb
@@ -0,0 +1,22 @@
+require 'polipus'
+
+Polipus.crawler('rubygems', 'http://rubygems.org/') do |crawler|
+  # Handle connectivity errors
+  # Only runs when there is an error
+  polipus.on_page_error do |page|
+    # Don't store the page
+    page.storable = false
+    # Add the URL again to the queue
+    polipus.add_to_queue(page)
+  end
+
+  # In-place page processing
+  # Runs also when there was an error in the page
+  crawler.on_page_downloaded do |page|
+    # Skip block if there is an error
+    return if page.error
+
+    # A nokogiri object
+    puts "Page title: '#{page.doc.at_css('title').content}' Page url: #{page.url}"
+  end
+end
