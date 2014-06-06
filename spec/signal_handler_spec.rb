70108e94d0f0ed2f89e0459c294832a8824a693b
Francesco Laurita <taganaka@gmail.com>
2014-06-06 12:07:10 -0700

    spec added

---
+++ spec/signal_handler_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe Polipus::SignalHandler do
+
+  context 'signal handler' do
+  
+    it 'should be enabled by default' do
+      Polipus::PolipusCrawler.new("polipus-rspec", [])
+      Polipus::SignalHandler.enabled?.should be true
+    end
+
+    it 'should be disabled if specified' do
+      Polipus::PolipusCrawler.new("polipus-rspec", [], enable_signal_handler: false)
+      Polipus::SignalHandler.enabled?.should be false
+    end
+
+  end
+end
